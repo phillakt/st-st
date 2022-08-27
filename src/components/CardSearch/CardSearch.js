1b67505e29493dd4a2b6d456a9b12417dc7355c3
phillakt <phillakt@yandex.ru>
2022-08-27 14:10:46 +0300

    02 - блок поиска

---
+++ src/components/CardSearch/CardSearch.js
@@ -0,0 +1,14 @@
+import React from "react";
+import style from "./CardSearch.module.scss";
+
+const CardSearch = () => {
+    return  (
+        <div className="">
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
+    );
+}
+
+export default CardSearch;
